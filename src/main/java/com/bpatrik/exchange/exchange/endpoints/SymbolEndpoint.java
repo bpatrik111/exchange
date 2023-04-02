7d6cac884d93e271c70dfe783a1913cf5671826f
bpatrik111 <bpatrik111@gmail.com>
2023-04-02 15:11:05 +0200

    Exceptions

---
+++ src/main/java/com/bpatrik/exchange/exchange/endpoints/SymbolEndpoint.java
@@ -0,0 +1,19 @@
+package com.bpatrik.exchange.exchange.endpoints;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/symbols")
+public class SymbolEndpoint {
+
+
+    @GetMapping("")
+    public ResponseEntity<List<Symbol>> getAllSymbol(HttpServletRequest request) {
+        int userId = (Integer) request.getAttribute("userId");
+        List<Category> categories = categoryService.fetchAllCategories(userId);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+}
